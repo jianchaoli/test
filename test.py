da5fbec934bf76b2bf97f4399fa1c9c21bf18a72
John Doe <1374176390@qq.com>
2015-03-25 22:27:06 +0800

    ljc first commit

--- test.py
+++ test.py
@@ -1,2 +1,25 @@
-import os
 
+vlist = []
+timelist = []
+def fun(v, t, T = 0):
+	from math import e
+	mlist = []
+	if T != 0:
+		
+		vlist.append(v)
+		timelist.append(t)	
+	else:
+		vlist.append(v)
+		timelist.append(t)
+		for i in range(len(vlist)):
+			vlist[i] = vlist[i] * (e**(timelist[i]-T))
+		for j in range(len(vlist)):
+			mlist.append(vlist[j] // 1)
+		myset = set(mlist)
+		for item in myset:
+			print("%d ~ %d: %d" %(item,item+1,mlist.count(item)))	
+ 
+k = 4
+for j in range(4):
+	k -= 1
+	fun(2, 3, k)
